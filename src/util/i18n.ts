c329a0f43089299d3aba760ff028f05cdf5fe620
VALDEUS <valdeuscorp@gmail.com>
2025-07-29 19:38:09 +0300

    style: format i18n initialization

--- src/util/i18n.ts
+++ src/util/i18n.ts
@@ -1,9 +1,9 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
+import i18n from "i18next"
+import { initReactI18next } from "react-i18next"
+import LanguageDetector from "i18next-browser-languagedetector"
 
-import en from "../locales/en/english.json";
-import uk from "../locales/uk/ukrainian.json";
+import en from "../locales/en/english.json"
+import uk from "../locales/uk/ukrainian.json"
 
 i18n
   .use(LanguageDetector)
@@ -18,6 +18,6 @@ i18n
       en: { translation: en },
       uk: { translation: uk },
     },
-  });
+  })
 
-export default i18n;
+export default i18n
