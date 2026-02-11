b3f2c6a15295e79db451dfdfd495c1664b625217
henry232323 <14969260+henry232323@users.noreply.github.com>
2026-02-11 09:38:36 -0800

    Update dependency

--- vite.config.ts
+++ vite.config.ts
@@ -376,5 +376,8 @@ export default defineConfig({
   server: {
     host: "0.0.0.0", // Listen on all network interfaces
     port: 5173,
+    watch: {
+      ignored: ["**/node_modules/**"],
+    },
   },
 })
