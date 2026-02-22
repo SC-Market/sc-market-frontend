7f90d272df3c89bd7dd92022caa6425d485c9b93
henry232323 <henry232323@users.noreply.github.com>
2026-02-21 18:04:21 -0800

    Add height: fit-content to contracts sidebar
    - Contracts page sidebar now only takes up needed height

--- src/pages/contracting/Contracts.tsx
+++ src/pages/contracting/Contracts.tsx
@@ -233,6 +233,7 @@ export function Contracts() {
                               position: "sticky",
                               top: "calc(64px + 16px)",
                               maxHeight: "calc(100vh - 64px - 32px)",
+                              height: "fit-content",
                               width: 300,
                               flexShrink: 0,
                               overflowY: "auto",
